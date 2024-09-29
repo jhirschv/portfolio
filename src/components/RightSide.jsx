f6b8ffa02c56c45b24c87523ea3618e8af538e8d
jhirschv <johnhirschv@gmail.com>
2024-09-29 13:47:24 -0500

    added featured project

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -46,7 +46,18 @@ const RightSide = () => {
           <BackgroundGradient className="rounded-[22px] bg-white dark:bg-zinc-900">
           <Card className='border-0 rounded-[22px]'> 
               <CardHeader className='pb-2'>
-                  <CardTitle>Train.io</CardTitle>
+                  <CardTitle className='flex justify-between'>
+                    <p>Train.io</p>
+                      <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+                      <span className="absolute inset-0 overflow-hidden rounded-full">
+                        <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+                      </span>
+                      <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 ">
+                        <span className='text-lg'>{`Featured Project`}</span>
+                      </div>
+                      <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+                    </button>
+                  </CardTitle>
                   <CardDescription>A better fitness app</CardDescription>
               </CardHeader>
               <CardContent className='flex flex-col gap-4 text-muted-foreground'>
